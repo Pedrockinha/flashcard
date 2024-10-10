ff93403af7b28021193ef2ed80b8f46ae241a929
Pedrockinha <152206225+Pedrockinha@users.noreply.github.com>
2024-10-10 07:52:17 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Arte',
+    'Quem pintou a Monalisa?',
+    'Leonardo da Vinci!',
+)
+
+criaCartao(
+    'Games',
+    'Qual é considerado o jogo mais bem avaliado da história?',
+    'The Legend o Zelda Ocarina of Time!',
+)
+
+criaCartao(
+    'Mitologia',
+    'Qual monstro mitológico só pode ser morto com uma bala de prata?',
+    'Lobisomem!',
+)
+
+criaCartao(
+    'Bíblia',
+    'Qual foi o primeiro milagre de Jesus?',
+    'Água trasnformada em vinho!',
+)
+
+criaCartao(
+    'Bíblia',
+    'Quem lutou contra o anjo?',
+    'Jacó',
+)
+
+criaCartao(
+    'Bíblia',
+    'Quem foi enviado por Deus para salvar o seu povo do Egito?',
+    'Moisés!',
+)
